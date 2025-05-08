07994c6ad384865869cc5b5555a34d7e19d2e91f
yann074 <yannsmith2907@gmail.com>
2025-05-08 00:04:37 -0300

    feat: cors q vai dar certo
    [

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,25 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://carangonde-front-production.up.railway.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -16,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
